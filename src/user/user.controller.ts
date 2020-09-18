a90ef564e8c69f6042951872e63207058d24827a
dbettenbuk <fade@hotmail.hu>
2020-09-18 15:45:58 +0200

    chore - remove unnecessary imports

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,11 @@
 import { Controller, Get, HttpStatus, UseGuards, Query } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiResponse, ApiQuery } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { UserService } from 'src/auth/user.service';
 import { UserDto } from './dtos/user.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { Role } from 'src/auth/enums/role.enum';
-import { ListQueryParams } from 'src/common';
 import { UserQueryParams } from 'src/auth/dtos/user-query-params';
 
 @ApiTags('user')
